c28c23a0bd3cac9ffae015935302475d5f13ffc6
alvinsun <alvinsunyixiao@gmail.com>
2016-05-16 11:23:45 +0800

    sync

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 import os,time
 from person import Person
+import serial
 import picamera
 cam = picamera.PiCamera()
 cam.resolution = (640,480)
-
+ser = serial.Serial('/dev/ttyACM0',9600)
 from person import recgonize
 
 try:
@@ -13,8 +14,13 @@ try:
         result = recgonize(cam)
         if result == None:
             print 'no face'
+            ser.write('noface')
             continue
         (name, confidence) = result
+        if confidence<=60:
+            ser.write('unkown')
+        else:
+            ser.write(name)
         print name + '    ' + str(confidence)
 except KeyboardInterrupt:
     cam.close()
